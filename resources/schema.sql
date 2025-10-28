8311f4104b45a83031829d7b93ef2720f277ea80
AnjiNight <nicolypaschoa7@gmail.com>
2025-10-28 20:04:23 -0300

    mudando squema
    Alterações de criação de tabelas

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,15 +1,25 @@
-CREATE TABLE rodada (
+CREATE TABLE IF NOT EXISTS startup (
+    nome VARCHAR(100) PRIMARY KEY,
+    caixa DOUBLE,
+    receita_base DOUBLE,
+    reputacao INT,
+    moral INT
+);
+
+CREATE TABLE IF NOT EXISTS rodada (
     id IDENTITY PRIMARY KEY,
     startup_nome VARCHAR(100),
     numero_rodada INT,
     receita DOUBLE,
     caixa DOUBLE,
-    receita_base DOUBLE
+    receita_base DOUBLE,
+    FOREIGN KEY (startup_nome) REFERENCES startup(nome)
 );
 
-CREATE TABLE decisao (
+CREATE TABLE IF NOT EXISTS decisao (
     id IDENTITY PRIMARY KEY,
     startup_nome VARCHAR(100),
     numero_rodada INT,
-    tipo_decisao VARCHAR(50)
+    tipo_decisao VARCHAR(50),
+    FOREIGN KEY (startup_nome) REFERENCES startup(nome)
 );
