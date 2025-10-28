e6eee8eb10f8b7cd9ae0809823ae0f65f3871c97
AnjiNight <nicolypaschoa7@gmail.com>
2025-10-28 18:10:20 -0300

    .

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -3,22 +3,29 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnectionFactory {
+
     private static final String URL = "jdbc:h2:./database/startupdb";
     private static final String USER = "sa";
     private static final String PASSWORD = "";
 
     static {
-    try {
-        Class.forName("org.h2.Driver");
-        try (Connection conn = getConnection()) {
-            java.sql.Statement stmt = conn.createStatement();
-            stmt.execute("RUNSCRIPT FROM 'src/dao/schema.sql'");
+        try {
+            Class.forName("org.h2.Driver");
+
+            // Cria o banco e executa o schema.sql se ainda não existir
+            try (Connection conn = getConnection()) {
+                Statement stmt = conn.createStatement();
+                stmt.execute("RUNSCRIPT FROM 'src/dao/schema.sql'");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao inicializar o banco: " + e.getMessage());
         }
-    } catch (Exception e) {
-        throw new RuntimeException("Erro ao carregar schema: " + e.getMessage());
-    }
     }
 
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
