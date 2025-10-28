c6a3e0b9f59316659361cbecc459321f61645c57
AnjiNight <nicolypaschoa7@gmail.com>
2025-10-28 17:51:00 -0300

    Changes

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,1 +1,15 @@
--- TODO: criar tabelas: startup, rodada, decisao_aplicada
+CREATE TABLE rodada (
+    id IDENTITY PRIMARY KEY,
+    startup_nome VARCHAR(100),
+    numero_rodada INT,
+    receita DOUBLE,
+    caixa DOUBLE,
+    receita_base DOUBLE
+);
+
+CREATE TABLE decisao (
+    id IDENTITY PRIMARY KEY,
+    startup_nome VARCHAR(100),
+    numero_rodada INT,
+    tipo_decisao VARCHAR(50)
+);
